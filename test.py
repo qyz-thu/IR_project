6f4865aa136b900e2eac17442bad1eae4659194b
qyz17 <qyz17@mails.tsinghua.edu.cn>
2020-12-12 16:43:33 +0800

    add function to calculate idf for terms appearing over 5 times

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from elasticsearch import Elasticsearch, helpers
 import time
+import re
+import json
 es = Elasticsearch()
 
 
@@ -43,6 +45,37 @@ def add_document(interval=100000):
                 print('time used %.2f s' % (time.time() - start_time))
 
 
+def get_vocab(file_path, output_path):
+    property_set = {'n', 'np', 'ns', 'ni', 'nz', 'q', 'mq', 't', 'f', 's', 'v', 'a', 'd', 'h', 'k', 'i', 'j', 'r', 'c', 'e', 'g'}
+    vocab = dict()
+    idf = dict()
+    pattern = re.compile('([^\s]+?)_([a-z]+?)')
+    with open(file_path, encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            if i % 100000 == 0:
+                print('processed %d lines' % i)
+            words = pattern.findall(line)
+            for word in words:
+                if word[1] not in property_set:
+                    continue
+                vocab[word[0]] = vocab[word[0]] + 1 if word[0] in vocab else 1
+            word_set = set([w[0] for w in words])
+            for word in word_set:
+                idf[word] = idf[word] + 1 if word in idf else 1
+        total_count = i
+
+    print(len(vocab))
+    filtered_vocab = [x for x in vocab if vocab[x] > 5]
+    print(len(filtered_vocab))
+    filtered_idf = {'doc_count': total_count}
+    for x in filtered_vocab:
+        filtered_idf[x] = idf[x]
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(filtered_idf, f)
+
+
 if __name__ == "__main__":
     # delete_all()
-    add_document(500000)
+    # add_document(500000)
+    # get_vocab('../rmrb_done', '../idf.json')
+    pass
